0763ca55b8cdcac81c37cdb3dfd70b6a9378cd12
angelsilalahi <77722400+angelsilalahi@users.noreply.github.com>
2021-03-23 02:42:25 +0700

    Add files via upload

---
+++ KonversiMataUang/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace KonversiMataUang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.numInput = new System.Windows.Forms.NumericUpDown();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(121, 23);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(496, 58);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Konversi Mata Uang";
+            this.labelTitle.Click += new System.EventHandler(this.labelTitle_Click);
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFrom.Location = new System.Drawing.Point(58, 119);
+            this.labelFrom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(75, 31);
+            this.labelFrom.TabIndex = 1;
+            this.labelFrom.Text = "Input";
+            this.labelFrom.Click += new System.EventHandler(this.labelFrom_Click);
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(64, 154);
+            this.comboBoxFrom.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(93, 24);
+            this.comboBoxFrom.TabIndex = 5;
+            this.comboBoxFrom.SelectedIndexChanged += new System.EventHandler(this.comboBoxFrom_SelectedIndexChanged);
+            // 
+            // numInput
+            // 
+            this.numInput.Location = new System.Drawing.Point(167, 154);
+            this.numInput.Margin = new System.Windows.Forms.Padding(4);
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(160, 22);
+            this.numInput.TabIndex = 3;
+            this.numInput.ValueChanged += new System.EventHandler(this.numInput_ValueChanged);
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTo.Location = new System.Drawing.Point(399, 119);
+            this.labelTo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(96, 31);
+            this.labelTo.TabIndex = 4;
+            this.labelTo.Text = "Output";
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(405, 154);
+            this.comboBoxTo.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(92, 24);
+            this.comboBoxTo.TabIndex = 5;
+            this.comboBoxTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxTo_SelectedIndexChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(505, 155);
+            this.textBoxOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(132, 22);
+            this.textBoxOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 306);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.labelFrom);
+            this.Controls.Add(this.labelTitle);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "A";
+            ((System.ComponentModel.ISupportInitialize)(this.numInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.NumericUpDown numInput;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox textBoxOutput;
+       
+    }
+}
+
---
+++ KonversiMataUang/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonversiMataUang
+{
+
+	public partial class Form1 : Form
+	{
+		private List<Currency> currencies;
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeCurrency();
+			SetUpComponent();
+			Convert();
+		}
+		private void CreateNewCurrency(string name, float rate)
+		{
+			currencies.Add(new Currency(name, rate));
+		}
+		private void InitializeCurrency()
+		{
+			// https://api.exchangeratesapi.io/latest?base=IDR
+			currencies = new List<Currency>();
+			CreateNewCurrency("IDR", 1.0f);
+			CreateNewCurrency("HKD", 0.0005391561f);
+			CreateNewCurrency("PHP", 0.0033741044f);
+			CreateNewCurrency("GBP", 0.0000500689f);
+			CreateNewCurrency("RUB", 0.0051418677f);
+			CreateNewCurrency("JPY", 0.0075626175f);
+			CreateNewCurrency("EUR", 0.0000583806f);
+			CreateNewCurrency("MYR", 0.0002851423f);
+			CreateNewCurrency("USD", 0.0000694203f);
+			CreateNewCurrency("SGD", 0.0000932338f);
+			CreateNewCurrency("AUD", 0.0000897017f);
+		}
+		
+		private void SetUpComponent()
+		{
+			foreach (Currency currency in currencies)
+			{
+				comboBoxFrom.Items.Add(currency.currencySymbol);
+				comboBoxTo.Items.Add(currency.currencySymbol);
+			}
+			numInput.Minimum = 0;
+			numInput.Maximum = decimal.MaxValue;
+			numInput.DecimalPlaces = 2;
+
+			comboBoxFrom.SelectedIndex = 0;
+			comboBoxTo.SelectedIndex = 0;
+
+			comboBoxFrom.SelectedIndexChanged += comboBoxFrom_SelectedIndexChanged;
+			comboBoxTo.SelectedIndexChanged += comboBoxTo_SelectedIndexChanged;
+
+			numInput.ValueChanged += numInput_ValueChanged;
+		}
+		private void numInput_ValueChanged(object sender, EventArgs e)
+		{
+			Convert();
+		}
+
+		private void comboBoxTo_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Convert();
+		}
+		private void comboBoxFrom_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Convert();
+		}
+		private Currency GetCurrencyByName(string name)
+		{
+			for (int i = 0; i < currencies.Count; i++)
+			{
+				if (currencies[i].currencySymbol == name)
+				{
+					return currencies[i];
+				}
+			}
+			return null;
+		}
+		private void Convert()
+		{
+			float money = (float)numInput.Value;
+			Currency inCurrency = GetCurrencyByName(comboBoxFrom.Text);
+			Currency outCurrency = GetCurrencyByName(comboBoxTo.Text);
+            if (outCurrency != null)
+            {
+				float output = inCurrency.ConvertTo(money, outCurrency);
+				textBoxOutput.Text = "" + output;
+            }
+		}
+
+        private void labelTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelFrom_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Currency
+	{
+		public string currencySymbol { get; }
+		private float rate;
+
+		public Currency(string name, float rate)
+		{
+			this.currencySymbol = name;
+			this.rate = rate;
+		}
+
+		public float ConvertTo(float money, Currency currency)
+		{
+			return money / rate * currency.rate;
+		}
+	}
+}
